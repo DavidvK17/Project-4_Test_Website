cc123907648e3730a9f1996b34af159edb40f1eb
DavidvK17 <david.sing7@gmail.com>
2022-01-30 11:33:25 +0100

    Implement smooth scrolling

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Here I borrow the HTML and CSS from a Tutorial and focus on advanced DOM Manipulation. Many of these tools can be done using frameworks and libraries, but I want to know how to implement them with just JavaScript in order to gain a deeper understanding of the language and DOM Manipulation.
 
-- Implementing smooth scrolling (to be implemented)
+- Implementing smooth scrolling
 - Event Propagation: Bubbling and Capturing (to be implemented)
 - DOM Traversing - Tabbed Component (to be implemented)
 - Intersection Observer API (to be implemented)
--- script.js
+++ script.js
@@ -29,3 +29,30 @@ document.addEventListener('keydown', function(e) {
     closeModal();
   }
 });
+
+// Implement smoorth scroll
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function(e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  //   Elemebt that was clicked
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  //   window.scrollTo({
+  //     left: s1coords.left + window.pageXOffset,
+  //     top: s1coords.top + window.pageYOffset,
+  //     behavior: 'smooth'
+  //   });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
